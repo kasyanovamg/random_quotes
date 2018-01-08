7515e59934243668674b3f11648b149a7cc1c8b9
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-08 19:24:43 +0400

    fix: changed http to https on the json file

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
 });
 
 function getQuote() {
-	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+	var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function(json) {
         	if (json.quoteAuthor === "") {
         		json.quoteAuthor = "Unknown Author";
         	}
   			var html = "";
 
-  			html += "<div class = 'cat'>";	
+  			html += "<div class = 'quotes'>";	
     		html += "<strong>" + json.quoteAuthor + "</strong>: " + json.quoteText + "<br>";  	
   			html += "</div><br>";
 			
