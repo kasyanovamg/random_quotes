aeae5d0d601c39333c40a86378ba5f8c414b733a
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-08 19:19:13 +0400

    feat: tweet button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
   $(document).ready(function() {
-
+	getQuote()
     $("#generate").on("click", function(){
-      	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+      	getQuote()
+	});
+
+});
+
+function getQuote() {
+	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function(json) {
         	if (json.quoteAuthor === "") {
         		json.quoteAuthor = "Unknown Author";
@@ -11,11 +17,11 @@
   			html += "<div class = 'cat'>";	
     		html += "<strong>" + json.quoteAuthor + "</strong>: " + json.quoteText + "<br>";  	
   			html += "</div><br>";
-
 			
 			$(".quote").html(html);
-    	});
+    	
+    	$("#tweet").on("click", function(){
+      	$(this).attr("href", "https://twitter.com/intent/tweet?text=" + json.quoteText + " - " + json.quoteAuthor)
 	});
-});
-
-
+	});
+}
