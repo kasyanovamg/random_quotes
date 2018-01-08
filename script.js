dccd2b1a47d39ebca443ae29c738b1d2d81a3758
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-08 17:53:46 +0400

    feat: add json file

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-$(document).ready(function() {
-  $("#generate").on("click", function(){
-    // Only change code below this line.
-    $(".quote").html("The quote has changed.");
-    // Only change code above this line.
-  });
+  $(document).ready(function() {
+
+    $("#generate").on("click", function(){
+      // Only change code below this line.
+      $.getJSON("quotes.json", function(json) {
+  $(".quote").html(JSON.stringify(json));
 });
+      
+      
+      // Only change code above this line.
+    });
+
+  });
+
