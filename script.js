56cee8067f7aef015f71c10e8155f605c98faa89
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-08 19:03:04 +0400

    feat: Random Quotes appear properly on the screen

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
   $(document).ready(function() {
 
     $("#generate").on("click", function(){
-      console.log("click");
-      var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+      	var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function(json) {
-  		$(".quote").html(JSON.stringify(json));
+        	if (json.quoteAuthor === "") {
+        		json.quoteAuthor = "Unknown Author";
+        	}
+  			var html = "";
 
-		});
-      
-    });
+  			html += "<div class = 'cat'>";	
+    		html += "<strong>" + json.quoteAuthor + "</strong>: " + json.quoteText + "<br>";  	
+  			html += "</div><br>";
+
+			
+			$(".quote").html(html);
+    	});
+	});
 });
 
 
