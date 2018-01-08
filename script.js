b68bd42777fd9b1f4bb1beac6874422fa8439f06
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-08 18:46:50 +0400

    fix: JSON is displayed correctly now

--- script.js
+++ script.js
@@ -3,14 +3,12 @@
     $("#generate").on("click", function(){
       console.log("click");
       var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-      $.getJSON(url, function(json) {
-      	console.log("Get Json");
-  $(".quote").html(JSON.stringify(json));
-});
-      
-      
-      // Only change code above this line.
-    });
+        $.getJSON(url, function(json) {
+  		$(".quote").html(JSON.stringify(json));
 
-  });
+		});
+      
+    });
+});
+
 
