688cb29651095d5cc4ffe0f4a530201988d37ced
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-09 20:05:49 +0400

    feat: added css styles

--- script.js
+++ script.js
@@ -10,19 +10,25 @@ function getQuote() {
 	var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
         $.getJSON(url, function(json) {
         	if (json.quoteAuthor === "") {
-        		json.quoteAuthor = "Unknown Author";
+        		json.quoteAuthor = "Unknown";
         	}
   			var html = "";
 
-  			html += "<div class = 'quotes'>";	
-    		html += "<strong>" + json.quoteAuthor + "</strong>: " + json.quoteText + "<br>";  	
-  			html += "</div><br>";
-			
+  			html += "<div class = 'text'>";
+  			html += '<i class="fa fa-quote-left" aria-hidden="true"></i>' + ' ';
+  			html += json.quoteText;
+  			html += "</div><br>"
+  			html += "<div class = 'author'>";
+  			html += '<i class="fa fa-quote-right" aria-hidden="true"></i>' + ' ';
+  			html += json.quoteAuthor;
+  			html += "</div><br>"
+ 			
 			$(".quote").html(html);
     	
     	$("#tweet").on("click", function(){
-      	$(this).attr("href", "https://twitter.com/intent/tweet?text=" + json.quoteText + " - " + json.quoteAuthor)
-	});
+      		$(this).attr("href", "https://twitter.com/intent/tweet?text=" + json.quoteText + " ~ " + json.quoteAuthor);
+		});
+
 	});
     var color = randomColor();
 	$("body").css("background", color);
